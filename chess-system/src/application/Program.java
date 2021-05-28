d918b2ed05b6f4c30001775e8108b7171d5cfbe9
Ernane <ernane@spdata.com.br>
2021-05-28 12:14:26 -0300

    start to implement Board and Piece

--- chess-system/src/application/Program.java
+++ chess-system/src/application/Program.java
@@ -1,8 +1,13 @@
 package application;
 
+import boradGame.Board;
+
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Board board = new Board(8, 8);
+		
 		System.out.print("Ola");
 
 	}
